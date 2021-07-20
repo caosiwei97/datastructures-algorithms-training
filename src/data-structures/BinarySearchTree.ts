20e7c361228a45d338bf40a23de5a8e0a8c67391
caosiwei <1286612323@qq.com>
2021-07-20 16:11:25 +0800

    feat: finish binarySearchTree

--- src/data-structures/BinarySearchTree.ts
+++ src/data-structures/BinarySearchTree.ts
@@ -1,8 +1,9 @@
 import { Compare, defaultCompare, ICompareFunction } from '../util'
 import { Node } from './models/node'
 
+type INode<T> = Node<T> | null
 export default class BinarySearchTree<T> {
-  protected root: Node<T> | null = null
+  protected root: INode<T> = null
 
   constructor(protected compareFn: ICompareFunction<T> = defaultCompare) {}
 
@@ -41,36 +42,128 @@ export default class BinarySearchTree<T> {
     }
   }
 
-  // 查找一个键。如果节点存在，则返回 true；如果不存在，则返回false
-  // search(key: T): boolean
-
   // 中序遍历方式遍历所有节点。
   inOrderTraverse(callback: (param: T) => any) {
     this.inOrderTraverseNode(this.root, callback)
   }
 
-  protected inOrderTraverseNode(node: Node<T> | null, callback: (param: T) => any) {
+  protected inOrderTraverseNode(node: INode<T>, callback: (param: T) => any) {
     if (node !== null) {
-      // 递归遍历左节点
       this.inOrderTraverseNode(node.left, callback)
-      // 遍历根节点
       callback(node.key)
       this.inOrderTraverseNode(node.right, callback)
     }
   }
 
   // 先序遍历方式遍历所有节点
-  // preOrderTraverse()
+  preOrderTraverse(callback: (param: T) => any) {
+    this.preOrderTraverseNode(this.root, callback)
+  }
+
+  protected preOrderTraverseNode(node: INode<T>, callback: (param: T) => any) {
+    if (node !== null) {
+      callback(node.key)
+      this.preOrderTraverseNode(node.left, callback)
+      this.preOrderTraverseNode(node.right, callback)
+    }
+  }
 
   // 后序遍历方式遍历所有节点
-  // postOrderTraverse()
+  postOrderTraverse(callback: (param: T) => any) {
+    this.postOrderTraverseNode(this.root, callback)
+  }
+
+  protected postOrderTraverseNode(node: INode<T>, callback: (param: T) => any) {
+    if (node !== null) {
+      this.postOrderTraverseNode(node.left, callback)
+      this.postOrderTraverseNode(node.right, callback)
+      callback(node.key)
+    }
+  }
 
   // 树中最小的值/键
-  // min()
+  min() {
+    return this.minNode(this.root)
+  }
 
-  // 树中最大的值/键
-  // max()
+  protected minNode(node: INode<T>) {
+    let current = node
+    while (current !== null && current.left !== null) {
+      current = current.left
+    }
+    return current
+  }
 
-  // 从树中移除某个键
-  // remove(key)
+  /// 树中最大的值/键
+  max() {
+    return this.maxNode(this.root)
+  }
+
+  protected maxNode(node: INode<T>) {
+    let current = node
+    while (current !== null && current.right !== null) {
+      current = current.right
+    }
+    return current
+  }
+
+  // 查找一个键。如果节点存在，则返回 true；如果不存在，则返回false
+  search(key: T): boolean {
+    return this.searchNode(this.root, key)
+  }
+
+  protected searchNode(node: INode<T>, key: T): boolean {
+    if (node === null) {
+      return false
+    }
+    if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+      return this.searchNode(node.left, key)
+    }
+    if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
+      return this.searchNode(node.right, key)
+    }
+    return true
+  }
+
+  // 移除某个节点
+  remove(key: T) {
+    return this.removeNode(this.root, key)
+  }
+
+  protected removeNode(node: INode<T>, key: T): INode<T> {
+    if (node === null) {
+      return null
+    }
+    if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+      node.left = this.removeNode(node.left, key)
+      return node
+    } else if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
+      node.right = this.removeNode(node.right, key)
+      return node
+    } else {
+      // 找到值了
+      // 移除叶节点
+      if (node.left === null && node.right === null) {
+        node = null
+        return node
+      }
+      // 移除只有一个子节点的节点
+      if (node.left === null) {
+        node = node.right
+        return node
+      }
+      if (node.right === null) {
+        node = node.left
+        return node
+      }
+      // 移除有两个子节点的节点
+      // 找到该节点右子节点及后代最小的节点进行替换
+      const rightMinNode = this.minNode(node.right) as Node<T>
+
+      node.key = rightMinNode.key
+      // 移除最小节点
+      node.right = this.removeNode(node.right, rightMinNode.key)
+      return node
+    }
+  }
 }
